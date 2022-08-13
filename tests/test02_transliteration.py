15e83f64289abb973db7c46d0447547d5e9c4f74
Stefano Cossu <stefano@cossu.cc>
2022-08-12 19:53:43 -0700

    Parametrized tests in both directions.

--- tests/test02_transliteration.py
+++ tests/test02_transliteration.py
@@ -1,6 +1,6 @@
 import logging
 
-from unittest import TestCase
+from unittest import TestCase, TestSuite, TextTestRunner
 from csv import reader
 
 from importlib import reload
@@ -14,55 +14,64 @@ import transliterator.tables
 logger = logging.getLogger(__name__)
 
 
-class TestScriptToRoman(TestCase):
+class TestTrans(TestCase):
     """
     Test S2R transliteration.
 
     TODO use a comprehensive sample table and report errors for unsupported
     languages.
     """
-    def setUp(self):
-        if "TXL_CONFIG_TABLE_DIR" in environ:
-            del environ["TXL_CONFIG_TABLE_DIR"]
-            reload(transliterator.tables)
-            # import transliterator.tables
+    """
+    Modified test case class to run independent tests for each CSV row.
+    """
 
-    def test_basic_chinese(self):
-        src = "撞倒須彌 : 漢傳佛教青年學者論壇論文集"
-        dest = (
-                "Zhuang dao Xumi : han zhuan Fo jiao qing nian xue zhe lun "
-                "tan lun wen ji")
-
-        trans = transliterate(src, "chinese")
-        assert trans == dest
-
-    def test_available_samples(self):
+    def sample_s2r(self):
         """
-        Test all available samples for the implemented tables.
+        Test S2R transliteration for one CSV sample.
+
+        This function name won't start with `test_` otherwise will be
+        automatically run without parameters.
         """
-        for k, script, roman in _test_cases():
-            txl = transliterate(script, k)
-            if txl != roman:
-                warn_str = f"Mismatching transliteration in {k}!"
-                logger.warning("*" * len(warn_str))
-                logger.warning(warn_str)
-                logger.warning("*" * len(warn_str))
-                logger.info(f"Transliterated string: {txl}")
-                logger.info(f"        Target string: {roman}")
+        txl = transliterate(self.script, self.tbl)
+        self.assertEqual(txl, self.roman)
 
-            # assert txl == roman
+    def sample_r2s(self):
+        """
+        Test R2S transliteration for one CSV sample.
+
+        This function name won't start with `test_` otherwise will be
+        automatically run without parameters.
+        """
+        txl = transliterate(self.roman, self.tbl, r2s=True)
+        self.assertEqual(txl, self.script)
 
 
-def _test_cases():
-    test_cases = []
+def make_suite():
+    """
+    Build parametrized test cases.
+    """
+    suite = TestSuite()
     with open(
             path.join(TEST_DATA_DIR, "sample_strings.csv"),
             newline="") as fh:
         csv = reader(fh)
         csv.__next__()  # Discard header row.
+
         for row in csv:
             if len(row[2]):
-                # Table key, script, Roman
-                test_cases.append((row[2], row[3], row[4]))
+                # Inject transliteration info in the test case.
+                for tname in ("sample_s2r", "sample_r2s"):
+                    tcase = TestTrans(tname)
+                    tcase.tbl = row[2]
+                    tcase.script = row[3]
+                    tcase.roman = row[4]
+                    suite.addTest(tcase)
 
-    return test_cases
+    return suite
+
+
+if "TXL_CONFIG_TABLE_DIR" in environ:
+    del environ["TXL_CONFIG_TABLE_DIR"]
+    reload(transliterator.tables)
+
+TextTestRunner().run(make_suite())
