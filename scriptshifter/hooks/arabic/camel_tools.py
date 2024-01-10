0eb119866cf2e71842d9a9f2d79a5480dc7d815f
Stefano Cossu <4306733+scossu@users.noreply.github.com>
2024-01-10 08:48:54 -0500

    Arabic via CAMeL tools (#88)
    * Remove old Arabic transliterator submodule.
    
    * Add camel-tools submodule.
    
    * Add pip dependency on Camel tools.
    
    * WIP Camel Tools setup.
    
    * Use camel_tools from pip; add Arabic romanizer module.
    
    * Use Camel ALA Romanizer with mini model.
    
    * WIP add steps for model building and trainig.
    
    * WIP add partial Bash script to create & train model.

---
+++ scriptshifter/hooks/arabic/camel_tools.py
@@ -0,0 +1,32 @@
+from os import path
+from sys import path as syspath
+
+from scriptshifter import APP_ROOT
+from scriptshifter.exceptions import BREAK
+
+
+CAMEL_DIR = path.join(path.dirname(APP_ROOT), "ext", "arabic_rom")
+MODULE_DIR = path.join(CAMEL_DIR, "src")
+# MODEL_DIR = path.join(CAMEL_DIR, "models", "mle")
+MODEL_DIR = path.join(CAMEL_DIR, "data", "processed")
+# MODEL_PATH = path.join(MODEL_DIR, "size1.0.tsv")
+MODEL_PATH = path.join(MODEL_DIR, "train.tsv")
+
+syspath.append(MODULE_DIR)
+
+
+def s2r_post_config(ctx):
+    from predict import mle_predict as mle
+    from predict import translit_rules as tr
+
+    loc_exceptional = tr.load_exceptional_spellings()
+    loc_mappings = tr.load_loc_mappings()
+
+    mle_model = mle.load_mle_model(mle_model_tsv=MODEL_PATH)
+    ctx.dest = mle.apply_mle_translit_simple_backoff(
+            ctx.src,
+            mle_model,
+            loc_mappings,
+            loc_exceptional)
+
+    return BREAK
